3e8505f35003d4bffcb623443ae249fe820ff5dc
Fabio Baptista <24209013+fabiobap@users.noreply.github.com>
2024-09-27 23:36:05 -0300

    seeder for restrictions and rooms

---
+++ migrations/20240928023359_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,16 @@
+INSERT INTO
+    public.rooms (
+        room_name,
+        created_at,
+        updated_at
+    )
+VALUES (
+        'General Quarters',
+        '2024-09-24 00:00:00.000',
+        '2024-09-24 00:00:00.000'
+    ),
+    (
+        'Major''s Suite',
+        '2024-09-24 00:00:00.000',
+        '2024-09-24 00:00:00.000'
+    );
---
+++ migrations/20240928023512_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,11 @@
+INSERT INTO
+    public.restrictions (
+        restriction_name,
+        created_at,
+        updated_at
+    )
+VALUES (
+        'Reservation',
+        '2024-09-25 00:00:00.000',
+        '2024-09-25 00:00:00.000'
+    );
