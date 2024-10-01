73d08fa8e7073c612ca46d0f0fd79ee7a9506c48
Fabio Baptista <24209013+fabiobap@users.noreply.github.com>
2024-10-01 16:05:52 -0300

    login logic also admin logic started

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -8,9 +8,14 @@ import (
 
 type DatabaseRepo interface {
 	AllUsers() bool
+
 	InsertReservation(res models.Reservation) (int, error)
 	InserRoomRestriction(res models.RoomRestriction) error
 	SearchAvailabilityByDatesByRoomID(start, end time.Time, roomID int) (bool, error)
 	SearchAvailabilitForAllRooms(start, end time.Time) ([]models.Room, error)
 	GetRoomByID(id int) (models.Room, error)
+
+	GetUserByID(id int) (models.User, error)
+	UpdateUser(u models.User) error
+	Authenticate(email, testPassword string) (int, string, error)
 }
