9228f66a646f903f23da7761504e1bd07fbb776a
Fabio Baptista <24209013+fabiobap@users.noreply.github.com>
2024-10-03 15:03:33 -0300

    get all rooms func
    also calendar tmpl skeleton

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -546,6 +546,9 @@ func (m *Repository) AdminCalendarReservations(w http.ResponseWriter, r *http.Re
 		now = time.Date(year, time.Month(month), 1, 0, 0, 0, 0, time.UTC)
 	}
 
+	data := make(map[string]interface{})
+	data["now"] = now
+
 	next := now.AddDate(0, 1, 0)
 	last := now.AddDate(0, -1, 0)
 
@@ -564,8 +567,26 @@ func (m *Repository) AdminCalendarReservations(w http.ResponseWriter, r *http.Re
 	stringMap["this_month"] = now.Format("01")
 	stringMap["this_month_year"] = now.Format("2006")
 
+	currentYear, currentMonth, _ := now.Date()
+	currentLocation := now.Location()
+	firstOfMonth := time.Date(currentYear, currentMonth, 1, 0, 0, 0, 0, currentLocation)
+	lastOfMonth := firstOfMonth.AddDate(0, 1, -1)
+
+	intMap := make(map[string]int)
+	intMap["days_in_month"] = lastOfMonth.Day()
+
+	rooms, err := m.DB.AllRooms()
+	if err != nil {
+		helpers.ServerError(w, err)
+		return
+	}
+
+	data["rooms"] = rooms
+
 	render.Template(w, r, "admin-calendar-reservations.page.tmpl", &models.TemplateData{
 		StringMap: stringMap,
+		Data:      data,
+		IntMap:    intMap,
 	})
 }
 
--- internal/render/render.go
+++ internal/render/render.go
@@ -18,15 +18,37 @@ import (
 var app *config.AppConfig
 var pathToTemplates = "./templates"
 var functions = template.FuncMap{
-	"humanDate": HumanDate,
+	"humanDate":  HumanDate,
+	"formatDate": FormatDate,
+	"iterate":    Iterate,
+	"add":        Add,
 }
 
 func NewRenderer(ac *config.AppConfig) {
 	app = ac
 }
 
+func Add(a, b int) int {
+	return a + b
+}
+
+func Iterate(count int) []int {
+	var i int
+	var items []int
+
+	for i = 0; i < count; i++ {
+		items = append(items, i)
+	}
+
+	return items
+}
+
 func HumanDate(t time.Time) string {
-	return t.Format("2006-01-02")
+	return FormatDate(t, "2006-01-02")
+}
+
+func FormatDate(t time.Time, f string) string {
+	return t.Format(f)
 }
 
 func AddDefaultData(td *models.TemplateData, r *http.Request) *models.TemplateData {
